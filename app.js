6ab577e38d3a7c0d9071802fdcfe6bce05f40d9a
JessicaMotaB <motajessicaa@gmail.com>
2022-10-17 15:55:36 +1300

    Add edit option

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const _ = require("lodash");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const { render } = require("express/lib/response");
 
 
 const app = express();
@@ -90,14 +91,12 @@ app.get("/", async function(req, res){
   if (req.isAuthenticated()){
     const posts = await Post.find({userId: req.user.id})
     res.render("home",{
-      startingContent: homeStartingContent,
-      posts: posts,
+      posts: posts.reverse(),
       isAuthenticated: req.isAuthenticated()
     });
   } else {
     res.redirect("welcome");
   };
-  
 });
 
 app.get("/welcome", function(req, res){
@@ -113,7 +112,6 @@ app.get("/auth/google",
     // Successful authentication, redirect home.
     res.redirect("/");
   });
-
 
 
 app.get("/login", function(req, res){
@@ -162,17 +160,21 @@ app.post("/login", function(req, res){
   });
 });
 
-
-app.get("/compose", function(req, res){
-  Post.find({}, function(err, posts){
-   res.render("home", {
-     startingContent: homeStartingContent,
-     posts: posts,
-     isAuthenticated: req.isAuthenticated()
-     });
- });
-  res.render("compose", { isAuthenticated: req.isAuthenticated()});
+// Compose
+app.get("/compose", function(req, res){ 
+  if(req.isUnauthenticated()) {
+    res.redirect("/welcome")
+  } else {
+    res.render("compose", { 
+      isAuthenticated: req.isAuthenticated(),
+      postTitle: null,
+      postBody: null,
+      postId: null
+    });
+  }
+  
 });
+
 
 app.post("/compose", function(req, res){
   if(req.isUnauthenticated()) {
@@ -189,18 +191,86 @@ app.post("/compose", function(req, res){
          res.redirect("/");
        }
      });
-  }
+  };
 });
+
+//  UPDATE
+app.post("/posts/:postId/update", function(req, res){
+
+  if(req.isUnauthenticated()) {
+    res.redirect("/welcome")
+  } else {
+    const filter = {_id: req.body.postId};
+    const update = {title: req.body.postTitle, content: req.body.postBody}
+    Post.findOneAndUpdate(filter, update, function(err, post){
+      console.log(post);
+      if (err){
+        res.render("edit");
+      } else {
+        res.redirect(`/posts/${post._id}`);
+      }
+    });
+  };
+});
+
+//show post
 app.get("/posts/:postId", function(req, res){
-  const requestedPostId = req.params.postId;
-  Post.findOne({_id:requestedPostId}, function(err, post){
-    console.log(post);
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-  });
+  if (req.isAuthenticated()){
+    const requestedPostId = req.params.postId;
+    Post.findOne({_id:requestedPostId}, function(err, post){
+      console.log(post);
+        res.render("post", {
+          title: post.title,
+          content: post.content,
+          isAuthenticated: req.isAuthenticated(),
+          postId: post.id
+        });
+    });
+  } else {
+    res.redirect("/welcome");
+  };
 });
+
+//  DELETE
+app.post("/posts/:postId", function(req, res){
+  if (req.isAuthenticated()){
+    const requestedPostId = req.params.postId;
+    Post.findByIdAndRemove({_id:requestedPostId}, function(err, post){
+      if(!!err) {
+        res.render("post", {
+          title: post.title,
+          content: post.content,
+          isAuthenticated: req.isAuthenticated()
+        });
+      } else { 
+       return res.redirect("/")
+      }
+    });
+  } else {
+    res.redirect("/welcome");
+  };
+});
+
+// Edit Post 
+app.get("/posts/:postId/edit", function(req, res){
+  if (req.isAuthenticated()){
+    const requestedPostId = req.params.postId;
+    Post.findOne({_id:requestedPostId}, function(err, post){
+      console.log(post);
+        res.render("edit", {
+          postTitle: post.title,
+          postBody: post.content,
+          isAuthenticated: req.isAuthenticated(),
+          postId: post.id
+        });
+    });
+  } else {
+    res.redirect("/welcome");
+  };
+});
+
+
+
 app.get("/about", function(req, res){
   res.render("about", {
     aboutContent: aboutContent, 
