e7f1aa7d0babef546a2eae87db935f494bc59258
motajessica <motajessicaa@gmail.com>
2023-01-16 16:36:23 +1300

    Add 'Back to home' link to compose page

--- app.js
+++ app.js
@@ -289,12 +289,12 @@ app.post("/login", function(req, res){
   
   req.login(user, function(err){
     if (err) {
-      console.log(err);
+      // console.log(err);
       res.redirect("/login");
     } else {
       passport.authenticate("local")(req, res, function(){
-      res.redirect("/");
-    });
+        res.redirect("/");
+      });
     };
   });
 });
