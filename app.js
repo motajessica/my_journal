f394ae6848f6040e0487dd82fcc7555a57ac12ff
Vinicius <vinicius.bolzani001@msd.govt.nz>
2023-01-23 21:25:59 +1300

    Some UI improvements

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const _ = require("lodash");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 const { render } = require("express/lib/response");
@@ -222,14 +219,12 @@ app.get("/posts/:postId/edit", function(req, res){
 
 app.get("/about", function(req, res){
   res.render("about", {
-    aboutContent: aboutContent, 
     isAuthenticated: req.isAuthenticated()
   });
 });
 
 app.get("/contact", function(req, res){
   res.render("contact", {
-    contactContent: contactContent,
     isAuthenticated: req.isAuthenticated()
   });
 });
