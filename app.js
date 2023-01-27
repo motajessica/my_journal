d39b84e8d203e1b2acb7efb98217251d91e916e1
Vinicius <vinicius.bolzani001@msd.govt.nz>
2023-01-28 08:19:43 +1300

    Change prod db config

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://0.0.0.0:27017/blogDB", {useNewUrlParser: true}).then(() => {
+const DB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/blogDB"
+mongoose.connect(DB_URI, {useNewUrlParser: true}).then(() => {
   console.log("Connected to Database");
   }).catch((err) => {
       console.log("Not Connected to Database ERROR! ", err);
@@ -340,4 +341,4 @@ function getDate() {
     day: "numeric",
     month: "long"
   }));
-};
+}
